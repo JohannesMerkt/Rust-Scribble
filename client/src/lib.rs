d7ff3c0adfc4f491a7ad8d608cfb6a083908c11d
Terence Stenvold <tstenvold@gmail.com>
2022-06-15 08:54:07 +0200

    Removing lib.rs because we aren't building a library

--- client/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![crate_name = "rust_scribble"]
-mod app;
-pub use app::TemplateApp;
-mod painting;
-pub use painting::Painting;
-mod network;
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+mod app;
+pub use app::TemplateApp;
+mod painting;
+pub use painting::Painting;
+mod network;
 
 fn main() {
     tracing_subscriber::fmt::init();
@@ -7,6 +12,6 @@ fn main() {
     eframe::run_native(
         "Scribble",
         native_options,
-        Box::new(|_| Box::new(rust_scribble::TemplateApp::new())),
+        Box::new(|_| Box::new(TemplateApp::new())),
     );
 }
